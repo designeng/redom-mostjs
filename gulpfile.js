289e79406bf9453166a335945cbfa0a369ceab1a
designeng <denissavenok@gmail.com>
2020-02-28 01:26:47 +0300

    Check soften & tabyfy startegies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const soften = require('gulp-soften');
+const tabify = require('gulp-tabify');
 const rimraf = require('rimraf')
 const { promisify } = require('util');
 
@@ -7,7 +8,14 @@ const rimrafP = promisify(rimraf);
 
 gulp.task('soften', async function () {
     await rimrafP('./build');
-    await gulp.src('js/**/*.js')
+    await gulp.src('./js/**/*.js')
         .pipe(soften(8))
         .pipe(gulp.dest('build'))
-})
+});
+
+gulp.task('tabify', async function () {
+    await rimrafP('./build');
+    return gulp.src('./js/**/*.js')
+        .pipe(tabify(4, true))
+        .pipe(gulp.dest('build'));
+});
