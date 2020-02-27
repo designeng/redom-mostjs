3c4c1adf587b416943ae768440982a205943bd4a
designeng <denissavenok@gmail.com>
2020-02-28 01:27:33 +0300

    Tabs

--- js/api.js
+++ js/api.js
@@ -2,22 +2,22 @@ import { listen } from './dispatch';
 import Store from '@redom/store';
 
 export const api = (app) => {
-  const store = new Store();
-  let updating;
+    const store = new Store();
+    let updating;
 
-  // Application logic:
-  listen(app, {
-    hello (subject) {
-      set('hello.subject', subject);
-    }
-  });
+    // Application logic:
+    listen(app, {
+    	hello (subject) {
+    	    set('hello.subject', subject);
+    	}
+    });
 
-  const set = (path, value) => {
-    store.set(path, value);
-    updating || (updating = window.requestAnimationFrame(() => {
-      updating = false;
+    const set = (path, value) => {
+	    store.set(path, value);
+	    updating || (updating = window.requestAnimationFrame(() => {
+	        updating = false;
 
-      app.update(store.get());
-    }));
-  };
+	        app.update(store.get());
+	    }));
+    };
 };
